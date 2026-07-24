cf7e2e8329695ef4a102defae665e67daef323ed
Hamid Reza Ghavami <hamidr.ghavami@gmail.com>
2026-07-24 08:51:05 +0300

    first changes

--- api/app.js
+++ api/app.js
@@ -1,2 +1,64 @@
 // Your web server code; it receives the 
 // checkout request and immediately publishes it to the Kafka queue.
+import express from 'express';
+import { kafka } from 'kafkajs';
+
+const app = express();
+app.use(express.json());
+
+const kafkaPost = process.env.KAFKA_POST || 'kafka:9092';
+
+const PORT = process.env.PORT || 3000;
+
+// initialized Kafka Client
+const kafka = new kafka({ 
+    clientID: 'api-server',
+    brokers: [`${KAFKA_BROKER}`],
+});
+
+// Create Producer Instance
+const producer = kafka.producer();
+
+async function kafka () {
+    const orderData = await producer.connect();
+}
+
+app.post('/order', async (req, res) => {
+    try {
+        const { userID, producerID, quantity } = req.body;
+
+        const orderData = { 
+            userID, 
+            producerID,
+            quantity,
+            createdAt: new Date().toString(), // convert into string
+        };
+
+        // Push instantly to Kafka queue
+        // 202 is for user knows we got no need to wait for DB
+        return res.status(202).json({
+            status: 'Order received!',
+            order: orderData,
+        });
+    } catch (error) { 
+        console.error('Error publishing order to Kafka:', error);
+        return res.status(500).json({ error: 'Internal server error'});
+    }
+});
+
+// Start Server after Kafka connects
+const startServer = async () => { 
+    try { 
+        await producer.connect();
+        console.log('Connected to Kafka Producer successfully');
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on the port ${PORT}`);
+        });
+    } catch (error) { 
+        console.log('Failed to start API producer:', error);
+        producer.exit(1);
+    }
+}
+
+startServer();
